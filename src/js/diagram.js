ef1418e2bc005692d2c77e0d05f598bb59fd1945
Ronja Norlén <rono2300@student.miun.se>
2024-02-20 15:38:39 +0100

    Infört data i cirkeldiagram

--- src/js/diagram.js
+++ src/js/diagram.js
@@ -3,6 +3,7 @@
 //Cirkeldiagram = 5 mest sökta programmen på miun ht23, namn och program
 
 import Chart from 'chart.js/auto';
+
 // Hämta data från webbtjänst
 
 const url = "https://studenter.miun.se/~mallar/dt211g/";
@@ -15,24 +16,25 @@ async function init () {
         const response = await fetch(url);
         let data = await response.json();
 
-         // Filtrera ut kurser
-      data = data.filter((course) => {
-        return course.type.includes("Kurs");
-    });
+     displayCourses(data);
+    displayPrograms(data);
 
-        // Sortera data
-        data.sort((a, b) => a.applicantsTotal - b.applicantsTotal).reverse();
-        data = data.slice(0, 6);
-
-
-        displayCourses(data);
-      //  displayPrograms(programs);
-    } catch {
+    } catch(e) {
+        console.log(e);
         document.getElementById("error").innerHTML = "<p>Något gick fel..</p>";
     }
 }
 
 function displayCourses(data) {
+
+      // Filtrera ut kurser
+      data = data.filter((course) => {
+        return course.type.includes("Kurs");
+    });
+
+        // Sortera antal sökande på kurserna
+        data.sort((a, b) => a.applicantsTotal - b.applicantsTotal).reverse();
+        data = data.slice(0, 6);
     
     let courseNames = [];
     let applicantsTotals = [];
@@ -45,7 +47,7 @@ function displayCourses(data) {
 
     // Diagram //
 
-const ctx = document.getElementById('myChart');
+const ctx = document.getElementById('courseChart');
 
 new Chart(ctx, {
     type: 'bar',
@@ -56,7 +58,7 @@ new Chart(ctx, {
             data: applicantsTotals,
             backgroundColor: 'rgba(54, 162, 235, 0.2)', // Bakgrundsfärg för staplarna
                 borderColor: 'rgba(54, 162, 235, 1)', // Kantfärg för staplarna
-                borderWidth: 1
+                borderWidth: 2
         }]
     },
     options: {
@@ -71,15 +73,53 @@ new Chart(ctx, {
 }
 
 
-// function displayPrograms(programs) {
-  //  const programsEl = document.getElementById("myChart2");
 
-    // Loopa igenom och skriv ut
-   // programs.forEach((program) => {
-    //    programsEl.innerHTML += `
-     //  <p>${program.type}</p>
-     //  <p>${program.admissionRound}</p>
-      // <p>${program.applicantsTotal}</p>
-       // `;
-   // });
-//}
+// Program
+
+function displayPrograms(data) {
+
+    // Filtrera ut program
+    data = data.filter((program) => {
+      return program.type.includes("Program");
+  });
+
+      // Sortera antal sökande på kurserna
+      data.sort((a, b) => a.applicantsTotal - b.applicantsTotal).reverse();
+      data = data.slice(0, 6);
+  
+  let programNames = [];
+  let applicantsTotals = [];
+
+  // Loopa igenom och skriv ut
+  data.forEach((program) => {
+      programNames.push(program.name);
+      applicantsTotals.push(program.applicantsTotal);
+  });
+
+  // Diagram //
+
+const ctx = document.getElementById('programChart');
+
+new Chart(ctx, {
+  type: 'pie',
+  data: {
+      labels: programNames,
+      datasets: [{
+          label: 'Mest sökta program',
+          data: applicantsTotals,
+          backgroundColor: 'rgba(54, 162, 235, 0.2)', // Bakgrundsfärg för staplarna
+              borderColor: 'rgba(54, 162, 235, 1)', // Kantfärg för staplarna
+              borderWidth: 2
+      }]
+  },
+  options: {
+      scales: {
+          y: {
+              beginAtZero: true // Y-axeln börjar vid noll
+          }
+      }
+  }
+});
+
+}
+
