07a9a3f3a0980e2c5623930140d04a7733b044ef
Ronja Norlén <rono2300@student.miun.se>
2024-02-22 16:40:36 +0100

    Färgjustering på diagram

--- src/js/diagram.js
+++ src/js/diagram.js
@@ -1,7 +1,10 @@
 // Stapeldiagram = namn på kurs + totalt antal sökande, 6 mest sökta kurserna
 //Cirkeldiagram = 5 mest sökta programmen på miun ht23, namn och program
-
+"use strict";
 import Chart from 'chart.js/auto';
+import { Colors } from 'chart.js';
+
+Chart.register(Colors);
 
 // Hämta data från webbtjänst
 
@@ -9,16 +12,16 @@ const url = "https://studenter.miun.se/~mallar/dt211g/";
 
 window.onload = init();
 
-async function init () {
+async function init() {
     try {
         // Fetch-anrop
         const response = await fetch(url);
         let data = await response.json();
 
-     displayCourses(data);
-    displayPrograms(data);
+        displayCourses(data);
+        displayPrograms(data);
 
-    } catch(e) {
+    } catch (e) {
         console.log(e);
         document.getElementById("error").innerHTML = "<p>Något gick fel..</p>";
     }
@@ -26,15 +29,15 @@ async function init () {
 
 function displayCourses(data) {
 
-      // Filtrera ut kurser
-      data = data.filter((course) => {
+    // Filtrera ut kurser
+    data = data.filter((course) => {
         return course.type.includes("Kurs");
     });
 
-        // Sortera antal sökande på kurserna
-        data.sort((a, b) => a.applicantsTotal - b.applicantsTotal).reverse();
-        data = data.slice(0, 6);
-    
+    // Sortera antal sökande på kurserna
+    data.sort((a, b) => a.applicantsTotal - b.applicantsTotal).reverse();
+    data = data.slice(0, 6);
+
     let courseNames = [];
     let applicantsTotals = [];
 
@@ -45,29 +48,52 @@ function displayCourses(data) {
     });
 
     // Diagram //
+    Chart.defaults.font.size = 14;
+    const ctx = document.getElementById('courseChart');
 
-const ctx = document.getElementById('courseChart');
-
-new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: courseNames,
-        datasets: [{
-            label: 'Mest sökta kurser',
-            data: applicantsTotals,
-            backgroundColor: 'rgba(54, 162, 235, 0.2)', // Bakgrundsfärg för staplarna
-                borderColor: 'rgba(54, 162, 235, 1)', // Kantfärg för staplarna
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: courseNames,
+            datasets: [{
+                label: 'Mest sökta kurser',
+                data: applicantsTotals,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 205, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(201, 203, 207, 0.2)'
+                ],
+                borderColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(255, 159, 64)',
+                    'rgb(255, 205, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(54, 162, 235)',
+                    'rgb(153, 102, 255)',
+                    'rgb(201, 203, 207)'
+                ],
                 borderWidth: 2
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true // Y-axeln börjar vid noll
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true // Y-axeln börjar vid noll
+                },
+                x: {
+                    ticks: {
+                        autoSkip: false, // Förhindrar att tick labels försvinner
+                        maxRotation: 50, // Roterar texten 0 grader (rakt ned)
+                        minRotation: 50, // Roterar texten 0 grader (rakt ned)
+                    }
+                }
             }
         }
-    }
-});
+    });
 
 }
 
@@ -79,45 +105,61 @@ function displayPrograms(data) {
 
     // Filtrera ut program
     data = data.filter((program) => {
-      return program.type.includes("Program");
-  });
+        return program.type.includes("Program");
+    });
 
-      // Sortera antal sökande på kurserna
-      data.sort((a, b) => a.applicantsTotal - b.applicantsTotal).reverse();
-      data = data.slice(0, 6);
-  
-  let programNames = [];
-  let applicantsTotals = [];
+    // Sortera antal sökande på kurserna
+    data.sort((a, b) => a.applicantsTotal - b.applicantsTotal).reverse();
+    data = data.slice(0, 5);
 
-  // Loopa igenom och skriv ut
-  data.forEach((program) => {
-      programNames.push(program.name);
-      applicantsTotals.push(program.applicantsTotal);
-  });
+    let programNames = [];
+    let applicantsTotals = [];
 
-  // Diagram //
+    // Loopa igenom och skriv ut
+    data.forEach((program) => {
+        programNames.push(program.name);
+        applicantsTotals.push(program.applicantsTotal);
+    });
 
-const ctx = document.getElementById('programChart');
+    // Diagram //
 
-new Chart(ctx, {
-  type: 'pie',
-  data: {
-      labels: programNames,
-      datasets: [{
-          label: 'Mest sökta program',
-          data: applicantsTotals,
-          backgroundColor: 'rgba(54, 162, 235, 0.2)', // Bakgrundsfärg för staplarna
-              borderColor: 'rgba(54, 162, 235, 1)', // Kantfärg för staplarna
-              borderWidth: 2
-      }]
-  },
-  options: {
-      scales: {
-          y: {
-              beginAtZero: true // Y-axeln börjar vid noll
-          }
-      }
-  }
-});
+    const ctx = document.getElementById('programChart');
+
+    new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: programNames,
+            datasets: [{
+                label: 'Mest sökta program',
+                data: applicantsTotals,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 205, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(201, 203, 207, 0.2)'
+                ],
+                borderColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(255, 159, 64)',
+                    'rgb(255, 205, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(54, 162, 235)',
+                    'rgb(153, 102, 255)',
+                    'rgb(201, 203, 207)'
+                ],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true // Y-axeln börjar vid noll
+                }
+            }
+        }
+    });
 
 }
